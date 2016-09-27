3a371b6eb90ffbab58db617539e981062cd542e7
buhrmi <buhrmi@gmail.com>
2016-09-28 03:30:19 +0900

    render using templates stored in models

--- src/components/app.vue
+++ src/components/app.vue
@@ -10,25 +10,36 @@
 </template>
 
 <script lang="coffee">
-######## TODO: These things should be synced with the API #####################
+######## TODO: Placeholder for data from API #####################
 card1 = 
+  id: 1
   template: '<p>Title: {{ card.title }}</p><p>Content: {{ card.content }}</p>'
   data:
     title: "This is a card title"
     content: "This is card content"
 card2 =
+  id: 2
   template: '<p>Title: {{ card.title }}</p><p>Content: {{ card.content }}</p>'
+  schema:
+    title:
+      type: 'text'
+    content:
+      type: 'html'
+    background:
+      type: 'image'
   data:
     title: "This is another title"
     content: "This is more content"
 
 page1 =
+  id: 1
   cards: [card1, card2]
-  template: '<html><head><title>{{ page.title }}</title></head><body><h1>This is blueprint text</h1>{{ content }}</body></html>'
+  template: '<html><head><title>{{ page.title }}</title></head><body><h1>This is blueprint text</h1><div id="content">{{ content }}</div><footer>This is the footer</footer></body></html>'
   data:
     title: 'Woot'
     
 site =
+  id: 1
   pages: [page1]
 ######################################################################
     
--- src/components/editor.vue
+++ src/components/editor.vue
@@ -2,20 +2,21 @@
   .editor(v-bind:class="{previewing: previewing}")
     .menu
       a.toggle_button(@click="togglePreview") {{ previewing ? 'Edit' : 'Preview' }}
-    .preview_wrapper
+    .preview_wrapper(@mousemove="updatedHoveredElement")
       browser-facade
       iframe#page
 </template>
 
 <style media="screen" lang="less" scoped>
   .preview_wrapper {
-    width: 100%;
-    margin: 0 auto;
-    transform: scale(0.8);
+    // width: 100%;
+    margin: 100px 60px;
+    // transform: scale(0.8);
     transition: all 0.3s;
     position: relative;
-    margin-top: 60px;
+    // margin-top: 60px;
   }
+
   .menu {
     height: 30px;
     box-shadow:  0px 5px 10px rgba(0,0,0,0.1);
@@ -38,6 +39,7 @@
     }
   }
   #page {
+    pointer-events: none;
     border: none;
     width: 100%;
     position: relative;
@@ -52,10 +54,12 @@
     }
     .preview_wrapper {
       transform: scale(1);
-      margin-top: 0;
+      // width: 100%;
+      margin: 0;
     }
     #page {
       top: -56px;
+      pointer-events: all;
     }
     .menu {
       top: -62px;
@@ -79,24 +83,49 @@
     data: ->
       previewing: true
       appName: 'Qurate Cloud Site Editor'
+      hoveredElement: null
     ready: ->
-      pageElement = document.getElementById('page')
-      pageDoc = pageElement.contentDocument
-      pageData = this.currentPage.data
-      # rendering is done using liquid. first, the content elements are rendered, then the blueprint is rendered.
-      contentHTML = ''
-      for card in this.currentPage.cards
-        cardLiquid = Liquid.parse(card.template)
-        cardHTML = cardLiquid.render({card: card.data, page: pageData})
-        contentHTML += cardHTML
-      blueprintTemplate = this.currentPage.template
-      blueprintLiquid = Liquid.parse(blueprintTemplate)
-      renderedHTML = blueprintLiquid.render({page: pageData, content: contentHTML})
-      # renderedPage = blueprintHTML.replace('{{ yield }}', cardHTML + cardHTML) # TODO: make it real
-      pageDoc.getElementsByTagName('html')[0].innerHTML = renderedHTML
-      document.title = pageDoc.title
-      page.style.height = pageDoc.body.scrollHeight + 'px'
+      this.renderPage()
+    watch: 
+      hoveredElement: (newElement, oldElement) ->
+        console.log('hovering ', newElement, oldElement)
+        oldElement.classList.remove('hovered') if oldElement
+        return unless newElement
+        newElement.classList.add('hovered')
     methods: 
+      renderPage: ->
+        pageElement = document.getElementById('page')
+        pageDoc = pageElement.contentDocument
+        pageData = this.currentPage.data
+        # rendering is done using liquid. first, the content elements are rendered, then the blueprint is rendered.
+        separator = '<div class="__card_separator"></div>'
+        contentHTML = separator
+        for card in this.currentPage.cards
+          cardLiquid = Liquid.parse(card.template)
+          cardHTML = cardLiquid.render({card: card.data, page: pageData})
+          contentHTML += cardHTML + separator
+        blueprintTemplate = this.currentPage.template
+        blueprintLiquid = Liquid.parse(blueprintTemplate)
+        renderedHTML = blueprintLiquid.render({page: pageData, content: contentHTML})
+        # renderedPage = blueprintHTML.replace('{{ yield }}', cardHTML + cardHTML) # TODO: make it real
+        pageDoc.getElementsByTagName('html')[0].innerHTML = renderedHTML
+        document.title = pageDoc.title
+        page.style.height = pageDoc.body.scrollHeight + 'px'
+      updatedHoveredElement: (event) ->
+        
+        pageDoc = document.getElementById('page').contentDocument
+        separators = pageDoc.getElementsByClassName('__card_separator')
+        pageY = event.clientY - document.getElementById('page').getBoundingClientRect().top
+        pageX = event.clientX - document.getElementById('page').getBoundingClientRect().left
+        for separator in separators
+          y = separator.getBoundingClientRect().top
+          if Math.abs(y - pageY) < 10
+            return this.hoveredElement = separator
+        
+        this.hoveredElement = null
+        
+      hidePageElements: ->
+        true
       togglePreview: ->
         this.previewing = !this.previewing
         
