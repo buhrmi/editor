6f281f05e03e0a33aaadcfbc7455b8af78227b24
buhrmi <buhrmi@gmail.com>
2016-09-30 20:01:34 +0900

    add some logging

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var state = {}; // TODO: store in database
 var connections = [];
 socketServer.on('connect', function(connection) {
   connections.push(connection);
+  console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' connected.');
   for (var key in state) {
     // TODO: only send what a user is allowed to access.
     if (state.hasOwnProperty(key)) {
@@ -23,19 +24,20 @@ socketServer.on('connect', function(connection) {
   }
   connection.on('message', function(message) {
     var json = JSON.parse(message.utf8Data);
-    
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' sent ' + message.utf8Data);
     // Save the new value. // TODO: check if user is allowed to change.
     state[json[0]] = json[1];
     
     for (var i = 0; i < connections.length; i++) {
       var otherCon = connections[i];
-      //if (otherCon == connection) continue;
+      if (otherCon == connection) continue;
       otherCon.sendUTF(message.utf8Data);
       // otherCon.sendUTF(JSON.stringify(['message','derp']))
     }
   });
   connection.on('close', function(reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    if (connections.indexOf(connection) != -1) connections.splice(connections.indexOf(connection), 1);
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
   });
 });
 console.log('Qurate Server running on port ' + port);
